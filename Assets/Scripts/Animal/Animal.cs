2b27822c3185555e420d4820fa3d8f6781821146
Andrei Raketa <raketa420311@gmail.com>
2023-12-26 04:00:34 +0300

    Animal update

---
+++ Assets/Scripts/Animal/Animal.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Animal : MonoBehaviour
+{
+    [SerializeField] private AnimalView _view;
+
+    public void Initialize()
+    {
+        
+    }
+
+    public void Destroy()
+    {
+        
+    }
+
+    public void Pick()
+    {
+        
+    }
+
+    public void Drop()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Animal/AnimalView.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class AnimalView : MonoBehaviour
+{
+    [SerializeField] private SkinnedMeshRenderer _meshRenderer;
+    [SerializeField] private Animator _animator;
+}
